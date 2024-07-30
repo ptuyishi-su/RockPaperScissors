4a31515d594bc5c869698e4772ff6dba3e2a1118
patrickT <patricktuyishime@Patricks-MacBook-Air.local>
2024-07-30 17:40:56 -0400

    corrected syntax and winner results

--- javascript.js
+++ javascript.js
@@ -1,18 +1,18 @@
 const Rock = document.querySelector("#rock-btn");
 const Paper = document.querySelector("#paper-btn");
 const Scissors = document.querySelector("#scissors-btn");
-const winnerText = document.getElementById("winner")
-const results = document.getElementById('resulstBoard')
-let humanScore = 0
-let botScore = 0
-const finished = document.createElement('div')
-const Restart = document.getElementById("restartBtn")
+const winnerText = document.getElementById("winner");
+const humanScoreDisplay = document.getElementById('humanScoreDisplay');
+const botScoreDisplay = document.getElementById('botScoreDisplay');
+const Restart = document.getElementById("restartBtn");
+
+let humanScore = 0;
+let botScore = 0;
 
 function botPlayer() {
   let choices = ["rock", "paper", "scissors"];
   let random = Math.floor(Math.random() * choices.length);
-  let randomChoice = choices[random];
-  return randomChoice;
+  return choices[random];
 }
 
 function humanPlayer(choice) {
@@ -22,32 +22,34 @@ function humanPlayer(choice) {
 
 function determineWinner(human, bot) {
   if (human === bot) {
-    winnerText.innerText= "It's a tie!";
+    winnerText.innerText = "It's a tie!";
   } else if (
     (human === "rock" && bot === "scissors") ||
     (human === "paper" && bot === "rock") ||
     (human === "scissors" && bot === "paper")
   ) {
-    winnerText.innerText= `You win ${human} beats ${bot}`
-    humanScore++
+    winnerText.innerText = `You win! ${human} beats ${bot}`;
+    humanScore++;
     humanScoreDisplay.innerText = `Human: ${humanScore}`;
   } else {
-    winnerText.innerText= `You Lose ${bot} beats ${human}`
+    winnerText.innerText = `You lose! ${bot} beats ${human}`;
     botScore++;
-    botScoreDisplay.innerText = `Bot: ${botScore}`;  }
-  endGame(humanScore, botScore)
+    botScoreDisplay.innerText = `Bot: ${botScore}`;
+  }
+  endGame(humanScore, botScore);
 }
 
-function endGame (humanScore, botScore) {
+function endGame(humanScore, botScore) {
   if (humanScore === 10 || botScore === 10) {
-    let winner = humanScore === 5 ? "Human" : "Bot";
+    let winner = humanScore === 10 ? "Human" : "Bot";
     winnerText.innerText = `${winner} wins the game!`;
     Rock.disabled = true;
     Paper.disabled = true;
     Scissors.disabled = true;
   }
 }
-Restart.addEventListener("click", function(){
+
+Restart.addEventListener("click", function() {
   Rock.disabled = false;
   Paper.disabled = false;
   Scissors.disabled = false;
@@ -56,7 +58,7 @@ Restart.addEventListener("click", function(){
   humanScoreDisplay.innerText = `Human: ${humanScore}`;
   botScoreDisplay.innerText = `Bot: ${botScore}`;
   winnerText.innerText = "";
-})
+});
 
 Rock.addEventListener("click", function() {
   humanPlayer("rock");
